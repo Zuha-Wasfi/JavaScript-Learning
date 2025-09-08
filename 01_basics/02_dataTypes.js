9dae07903150c651013250f3d61c047d006ebb0f
Zuha Wasfi <160380074+Zuha-Wasfi@users.noreply.github.com>
2025-09-08 20:48:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+       +
+        3) // code readability should be high
+
+console.log("Zuha")
+
+
+let name = "Zuha"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Zuha"); // type => string
+console.log(typeof age); // type => number
+console.log(typeof null); // type => object
+console.log(typeof undefined); // type => undefined
