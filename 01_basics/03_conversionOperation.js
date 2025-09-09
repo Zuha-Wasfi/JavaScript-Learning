5c21c447853a1a8c44dc2039ef181f7a0946f11a
Zuha Wasfi <160380074+Zuha-Wasfi@users.noreply.github.com>
2025-09-09 18:23:10 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,63 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+console.log("\n")
+
+// ********************************* Operations ****************************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log("\n")
+
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2**3); // 2 to the power 3 => 8
+console.log(2/3); // 0.6666666666666666
+console.log(2%3); // 2
+
+console.log("\n")
+
+let str1 = "hello"
+let str2 = " zuha"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("\n")
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); // 32
+
+console.log("\n")
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+console.log("\n")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
