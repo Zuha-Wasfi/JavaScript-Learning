f2662a2d27847cb49b2b21846ec0030758cefbd1
Zuha Wasfi <160380074+Zuha-Wasfi@users.noreply.github.com>
2025-09-09 20:26:08 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive (call by value)
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // false
+
+const bigNumber = 345467467345354564n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Abc", "Xyz", "Cde"]; // Array
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+} // Objests
+
+const myFunction = function(){
+    console.log("Hello World");
+} // Functions
